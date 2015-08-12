bb9fc78d6f55ea23f73d49f3a94bd8d663486b85
Maksim Ponomarenko <m.a.ponomarenko@yandex.ru>
2015-08-12 12:26:56 +0400

    добавил регистронезависимость

--- php/hw01/07.php
+++ php/hw01/07.php
@@ -3,9 +3,10 @@
 $my_str = "qwrweq";
 
 function func($my_str){
-    $j = strlen($my_str) - 1;
+    $new_str = strtolower($my_str);
+    $j = strlen($new_str) - 1;
     for($i = 0; $i <= $j; $i++, $j--){
-        if($my_str[$i] != $my_str[$j]) return false;
+        if($new_str[$i] != $new_str[$j]) return false;
     }
     return true;
 };
