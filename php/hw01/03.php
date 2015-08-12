351bc2c2daa2adb621ed8f5d2000501b98847af1
Maksim Ponomarenko <m.a.ponomarenko@yandex.ru>
2015-08-12 16:06:22 +0400

    3 task is complete

--- php/hw01/03.php
+++ php/hw01/03.php
@@ -8,5 +8,65 @@
  */
 //супер-функция с переменным числом параметров!
 function opachki(){
-	
+	$parameters = func_get_args();
+
+    if(count($parameters) < 3){
+        echo "Для выполнения функции передано недостаточно параметров. Передайте не менее 3 параметров.";
+    }
+
+    switch($parameters[0]){
+        case "+":
+            for($i = 1; $i < count($parameters); $i++){
+                for($k = $i + 1; $k < count($parameters); $k++){
+                    echo ($parameters[$i]+$parameters[$k])."<br/>";
+                }
+            }
+            break;
+        case "-":
+            for($i = 1; $i < count($parameters); $i++){
+                for($k = $i + 1; $k < count($parameters); $k++){
+                    echo ($parameters[$i]-$parameters[$k])."<br/>";
+                }
+            }
+            for($i = (count($parameters)-1); $i >= 0; $i--){
+                for($k = $i - 1; $k > 0; $k--){
+                    echo ($parameters[$i]-$parameters[$k])."<br/>";
+                }
+            }
+            break;
+        case "*":
+            for($i = 1; $i < count($parameters); $i++){
+                for($k = $i + 1; $k < count($parameters); $k++){
+                    echo ($parameters[$i]*$parameters[$k])."<br/>";
+                }
+            }
+           break;
+        case "/":
+            for($i = 1; $i < count($parameters); $i++){
+                for($k = $i + 1; $k < count($parameters); $k++){
+                    echo ($parameters[$i]/$parameters[$k])."<br/>";
+                }
+            }
+            for($i = (count($parameters)-1); $i >= 0; $i--){
+                for($k = $i - 1; $k > 0; $k--){
+                    echo ($parameters[$i]/$parameters[$k])."<br/>";
+                }
+            }
+           break;
+        case "%":
+            for($i = 1; $i < count($parameters); $i++){
+                for($k = $i + 1; $k < count($parameters); $k++){
+                    echo ($parameters[$i]%$parameters[$k])."<br/>";
+                }
+            }
+            for($i = (count($parameters)-1); $i >= 0; $i--){
+                for($k = $i - 1; $k > 0; $k--){
+                    echo ($parameters[$i]%$parameters[$k])."<br/>";
+                }
+            }
+           break;
+        default:
+           echo "Первым параметром должен быть символ арифметического действия!";
+    }
 };
+opachki("-", 1,2,3);
