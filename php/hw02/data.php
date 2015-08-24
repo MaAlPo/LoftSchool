02b07c6298bf251267aba92962d2873e7746491c
Maksim Ponomarenko <m.a.ponomarenko@yandex.ru>
2015-08-24 11:34:34 +0400

    обновил файлы

--- php/hw02/data.php
+++ php/hw02/data.php
@@ -1,7 +1,9 @@
 <?php
 require_once("connection.php");
 
-$sql_products = "SELECT * FROM Товары";
+$sql_products =  "SELECT `Категории товаров`.Title, `Товары`.Title, `Товары`.Mark, `Товары`.Price, `Товары`.Count
+                  FROM `Товары`
+                  LEFT JOIN `Категории товаров` ON `Товары`.Id_catalog = `Категории товаров`.Id";
 $result = $mysql->query($sql_products);
 
 $products = $result->fetch_all(MYSQL_ASSOC);
