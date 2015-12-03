8cbdb00bd388346be8c3de44cb257841b3d83833
Maksim Ponomarenko <m.a.ponomarenko@yandex.ru>
2015-12-03 12:03:42 +0300

    final

--- php/hw02/data.php
+++ php/hw02/data.php
@@ -1,13 +1,13 @@
 <?php
 require_once "connection.php";
 
-$sql_products =  "SELECT `Категории товаров`.Title AS Category, `Товары`.Title, `Товары`.Mark, `Товары`.Price, `Товары`.Count
-                  FROM `Товары`
-                  LEFT JOIN `Категории товаров` ON `Товары`.Id_catalog = `Категории товаров`.Id";
+$sql_products =  "SELECT `Category`.Title AS Category, `Company`.Title, `Items`.Mark, `Items`.Price, `Items`.Count
+                  FROM `Items`
+                  LEFT JOIN `Category` ON `Items`.Id_catalog = `Category`.Id
+                  LEFT JOIN `Company` ON `Items`.Id_company = `Company`.Id_company";
+
 $result = $mysql->query($sql_products);
-
 $products = $result->fetch_all(MYSQL_ASSOC);
-
 foreach($products as $product){
 
     echo "{$product['Category']}:<br/>{$product['Title']} {$product['Mark']} - {$product['Price']}.руб. ( {$product['Count']} шт.)<br/>";
